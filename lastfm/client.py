55cf5b414a6b6342c13d3338eca8dd7d932c1bb2
twitch0001 <twitch7443@protonmail.com>
2021-01-24 23:23:26 +0000

    Add setup.py

--- lastfm/client.py
+++ lastfm/client.py
@@ -21,7 +21,7 @@ async def json_or_text(response):
 
 class Client:
     def __init__(self, client_key: str, client_secret: str = None):
-        self._key = client_key 
+        self._key = client_key
         self._secret = client_secret
 
         self._session = None
@@ -33,41 +33,40 @@ class Client:
 
         headers = {
             "User-Agent": self.user_agent
-        } 
-        
+        }
+
         params = {
             "format": "json",
-            "api_key": self._key, 
+            "api_key": self._key,
             "method": request.path
         }
         params.update(request.extra)  # Update with method specific params
-        async with self._session.request(request.method, URL, params=params) as response:
+        async with self._session.request(request.method, URL, headers=headers, params=params) as response:
             data = await json_or_text(response)
             print(response.status)
-            
+
             if 200 <= response.status < 300:
                 return data
 
-            error = data.get("error") 
+            error = data.get("error")
             if error:
                 raise mapping.get(error, LastFMException)(error, data.get("message"))
-        
+
             return data
 
     # > User methods <
-    async def get_info(self, user: str=None):
+    async def get_info(self, user: str = None):
         """user.getInfo - user defaults to session auth"""
         return await self._request(Request("GET", "user.getInfo", user=user))
 
-    async def get_recent_tracks(self, user: str, *, limit: int=10):
+    async def get_recent_tracks(self, user: str, *, limit: int = 10):
         return await self._request(Request("GET", "user.getRecentTracks", user=user, limit=limit))
-    
-    async def get_top_tracks(self, user: str, period: str=None, *, limit: int=10, page: int=0):
+
+    async def get_top_tracks(self, user: str, period: str = None, *, limit: int = 10, page: int = 0):
         return await self._request(Request("GET", "user.getTopTracks", user=user, limit=limit))
 
-    async def get_top_artists(self, user: str, period: str=None, *, limit: int=10, page: int=0):
+    async def get_top_artists(self, user: str, period: str = None, *, limit: int = 10, page: int = 0):
         return await self._request(Request("GET", "user.getTopArtists", user=user, limit=limit))
 
-    async def get_top_albums(self, user: str, period: str=None, *, limit: int=10, page: int=0):
+    async def get_top_albums(self, user: str, period: str = None, *, limit: int = 10, page: int = 0):
         return await self._request(Request("GET", "user.getTopAlbums", user=user, limit=limit))
-
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="lastfm-py",
+    version="0.0.1",
+    author="twitch7443",
+    author_email="twitch7443@pm.me",
+    description="An asynchronous LastFM API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/twitch0001/lastfm-py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requries=">=3.6"
+)
